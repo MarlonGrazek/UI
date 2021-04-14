4742ada028fedca33b768eabb032dd55d05076df
Marlon Grazek <business.marlongrazek@gmail.com>
2021-04-14 19:20:47 +0200

    getSlot fix 5

--- src/main/java/com/marlongrazek/ui/UI.java
+++ src/main/java/com/marlongrazek/ui/UI.java
@@ -349,8 +349,7 @@ public class UI {
         }
 
         public Integer getSlot(Item item) {
-            Bukkit.broadcastMessage(items.toString());
-            for (Integer s : items.keySet()) if (s != null) if (items.get(s) == item) return s;
+            for (Integer s : items.keySet()) if (s != null) if (items.get(s).equals(item)) return s;
             return null;
         }
 
