ef5711a0975a31dbcf200c84b19e344809f9799c
Marlon Grazek <business.marlongrazek@gmail.com>
2021-08-10 17:54:56 +0200

    Only Top Inventory Click

--- src/main/java/com/marlongrazek/ui/UI.java
+++ src/main/java/com/marlongrazek/ui/UI.java
@@ -184,21 +184,20 @@ public class UI {
 
                                 e.setCancelled(!item.undefinedClicksAllowed());
                                 if (item.getClickAction() != null) item.getClickAction().accept(e.getClick());
-                            } else e.setCancelled(true);
+                            } else if(e.getView().getTopInventory() == e.getClickedInventory()) e.setCancelled(true);
                         } else {
                             if (player.getItemOnCursor() != null) {
                                 if (player.getItemOnCursor().equals(item.toItemStack())) {
                                     e.setCancelled(!item.undefinedClicksAllowed());
                                     if (item.getClickAction() != null) item.getClickAction().accept(e.getClick());
                                 } else e.setCancelled(true);
-                            } else e.setCancelled(true);
+                            } else if(e.getView().getTopInventory() == e.getClickedInventory()) e.setCancelled(true);
                         }
                     }
                 }
             }
         }
     }
-    
 
     public static class Item {
 
@@ -515,3 +514,4 @@ public class UI {
         }
     }
 }
+
