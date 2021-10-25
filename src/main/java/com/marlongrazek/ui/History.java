29afe1fcee571b5b4cdf49a31be20878ffbf5fc2
Marlon Grazek <business.marlongrazek@gmail.com>
2021-10-25 18:24:52 +0200

    History added

---
+++ src/main/java/com/marlongrazek/ui/History.java
@@ -0,0 +1,43 @@
+package com.marlongrazek.ui;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class History {
+
+    private final ArrayList<UI.Page> history = new ArrayList<>();
+    private final Player player;
+
+    public History(Player player) {
+        this.player = player;
+    }
+
+    public void addPage(UI.Page page) {
+        history.add(page);
+    }
+
+    public void removePage(int index) {
+        history.remove(history.size() - index);
+    }
+
+    public void clear() {
+        history.clear();
+    }
+
+    public UI.Page getPage(int index) {
+        return history.get(history.size() - (index + 1));
+    }
+
+    public ArrayList<UI.Page> get() {
+        return history;
+    }
+
+    public void open(int index) {
+        if (getPage(index) != null) {
+            UI.Page page = getPage(index);
+            for (int i = 0; i < index; i++) history.remove(history.size() - 1);
+            page.open(player);
+        } else player.closeInventory();
+    }
+}
